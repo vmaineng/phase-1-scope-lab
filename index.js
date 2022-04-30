87f43387f47934bfa7e864e95cfc91f646bdec89
Mai Vang <vmaineng@gmail.com>
2022-04-30 09:57:09 -0700

    commit tree eba9a9fa16f7a7eac5c068ecef23fbdc697aad82

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+   var customerName = 'bob'
+   customerName.toUpperCase();
+    }
+
+//console.log(customer);
+
+function setBestCustomer(){
+    var bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = 'mary'
+}
